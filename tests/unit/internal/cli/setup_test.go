ea8fe1ba50cc11a61ead07007b465bde25b6b492
nj.jo <jonamjun.dev@gmail.com>
2025-07-24 15:10:23 +0900

    [claudesquad] update from 'test-cleanup' on 24 Jul 25 15:10 KST (paused)

--- tests/unit/internal/cli/setup_test.go
+++
@@ -1,6 +0,0 @@
-package cli_test
-
-// This file previously contained tests for deprecated autosetup commands.
-// The autosetup functionality (setup all, setup wizard) has been removed
-// and is being rewritten with new implementation.
-// All tests for deprecated setup commands have been removed.
